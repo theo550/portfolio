75cea2336cbe7a95bef45273fbffcc3ba0767595
theo550 <theo.gaudin35@gmail.com>
2022-04-06 17:58:01 +0200

    fix security issue

--- src/components/Source.jsx
+++ src/components/Source.jsx
@@ -3,7 +3,7 @@ export default function Source() {
   return (
     <div className="command">
       <p>Théo Gaudin@portfolio % source</p>
-      <a className="App-link" href="https://github.com/theo550/portfolio" target='_blank'>https://github.com/theo550/portfolio</a>
+      <a className="App-link" href="https://github.com/theo550/portfolio" rel='noreferrer' target='_blank'>https://github.com/theo550/portfolio</a>
     </div>
   )
 }
